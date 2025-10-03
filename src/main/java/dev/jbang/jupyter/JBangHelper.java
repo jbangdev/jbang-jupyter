3867acfaebc411bb6e9e3434e3634e7459d25d63
Max Rydahl Andersen <max@xam.dk>
2025-10-03 10:39:39 +0200

    fix: lookup jbang in known locations + add help magic

--- src/main/java/dev/jbang/jupyter/JBangHelper.java
+++ src/main/java/dev/jbang/jupyter/JBangHelper.java
@@ -4,17 +4,74 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class JBangHelper {
+
+    /**
+     * Looks for nearest JBang executable in the following priority order:
+     * 
+     * Note: on windows it will look for jbang.cmd not jbang which is a shell script.
+     * 
+     * 1. $JBANG_HOME/bin/jbang
+     * 2. jbang in $PATH
+     * 3. ~/.jbang/bin/jbang
+     *
+     * If none of the above are found, it will return null.
+     * 
+     * @return absolute patht to a JBang executable or null if none is found
+     * @throws IOException
+     */
+    public static String findJBangExecutable() throws IOException {
+        boolean isWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+        String jbangExecutable = isWindows ? "jbang.cmd" : "jbang";
+            
+        // 1. $JBANG_HOME/bin/jbang
+        String jbangHome = System.getenv("JBANG_HOME");
+        if (jbangHome != null) {
+            var homePath = Path.of(jbangHome).resolve("bin/" + jbangExecutable);
+            if (Files.exists(homePath)) {
+                return homePath.toString();
+            }
+        }
+
+        // 2. jbang in $PATH
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv != null) {
+            for (String dir : pathEnv.split(File.pathSeparator)) {
+                Path execPath = Path.of(dir).resolve(jbangExecutable);
+                if (Files.exists(execPath)) {
+                    return execPath.toString();
+                }
+            }
+        }
+
+        // 3. ~/.jbang/bin/jbang
+        var homePath = Path.of(System.getProperty("user.home")).resolve(".jbang/bin/" + jbangExecutable);
+        if (Files.exists(homePath)) {
+            return homePath.toString();
+        }
+
+        // 4. none found
+        return null;
+    }
+
     public static List<String> getJBangResolvedDependencies(String scriptRef, String body, boolean inclAppJar) throws IOException {
         try {
-            ProcessBuilder pb = new ProcessBuilder("jbang", "info", "tools", scriptRef); // TODO: locate and install if need be
+            String jbangExecutable = findJBangExecutable();
+            if (jbangExecutable == null) {
+                throw new RuntimeException("JBang executable not found in $JBANG_HOME, $PATH, or ~/.jbang/bin. Please install JBang.");
+            }
+
+            ProcessBuilder pb = new ProcessBuilder(jbangExecutable, "info", "tools", scriptRef); // TODO: locate and install if need be
             pb.redirectErrorStream(false);
             Process process = pb.start();
 
